b6dcac2b1e8a56775e809a2fa76eda3ddfebf6e3
ldebele <lemidebele@gmail.com>
2023-09-13 20:27:18 +0300

    added predict api

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Identifying-Diseases-In-Chest-X-Rays-and-COVID-19-Detection
+# Identifying-COVID-19-Diseases-In-Chest-X-Rays
+
+<p align="center">
+  <img src="./reports/demo.png" alt="Demo Image">
+</p>
+
+## Table Content
+- [Datasets](#datasets)
+- [Getting Started](#getting-started)
+- [Author](#author)
+
+## Datasets
+## Getting Started
+Follow theses steps to set up the environment and run the application.
+1. Fork the repository [here](https://github.com/ldebele/Identifying-COVID-19-Diseases-In-Chest-X-Rays.git).
+2. Clone the forked repository.
+    ```bash
+    git clone https://github.com/<YOUR-USERNAME>/Identifying-COVID-19-Diseases-In-Chest-X-Rays.git
+    cd Identifying-COVID-19-Diseases-In-Chest-X-Rays.git
+    ```
+3. Run the application.
+    ```bash
+    docker-compose up
+    ```
+## Author
+- `Lemi Debela`
--- src/api/main.py
+++ src/api/main.py
@@ -1,12 +1,15 @@
-import os
 import glob
 import numpy as np
-import tensorflow as tf
+from PIL import Image
 
 import uvicorn
 from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image 
+from tensorflow.keras.applications.inception_v3 import preprocess_input 
 
 
 
@@ -17,36 +20,48 @@ class FileUpload(BaseModel):
     file: UploadFile
 
 
-class PredictedResponse(BaseModel):
-    disease_type: str
-
-
-
 @app.on_event("startup")
 def load_model():
     # Load the model
     global model
-    model_path = glob.glob('../models/*.h5')[0]
-    model = tf.tf.keras.models.load_model(model_path)
+    model_path = glob.glob('./*.h5')[0]
+    model = tf.keras.models.load_model(model_path)
+
+# redirect
+@app.get("/", include_in_schema=False)
+async def redirect():
+    return RedirectResponse("/docs")
 
 
-@app.get("/")
+@app.get("/home")
 def home():
     return {"message": "This is your home page"}
 
 
 @app.post("/predict")
-async def prediction(file: FileUpload):
-    # check the uploaded file
-    if file.content_type.startswith("image/"):
-        img = await file.read() 
-        pred = model.predict(img)
-        pred = model.predict()
-        pred = np.round(pred)
-        return {"Disease Type": str(pred)}
-    else:
-        return {"error": "Uploaded file is not an image"}
+def predict(file: UploadFile = File(...)):
+
+    # Check if the uploaded file
+    fileExtension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not fileExtension:
+        raise HTTPException(status_code=415, detail="Unsupported file provided.")
+
+    # Read and preprocess the uploaded image
+    img = Image.open(file.file)
+    img = img.resize((224, 224))
+    img = img.convert("RGB")
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+
+    # predict the image
+    predict_coef = model.predict(img)
+    disease_type = ["COVID" if predict_coef >= 0.25 else "Non-COVID"]
+
+    return {"Disease Type": disease_type}
 
 
-if __name__ == "__main__"
+
+
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
